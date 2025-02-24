a0568e1b37b0d1072a8c8778cd41b6833f1e97fe
Andrew Johnson <johnsonrobertandrew@gmail.com>
2025-02-24 15:01:21 -0600

    fix channel fetching

--- app/actions.ts
+++ app/actions.ts
@@ -1,37 +1,45 @@
 "use server";
 
-import { Client, GatewayIntentBits, GuildChannel } from "discord.js";
+import { REST } from "@discordjs/rest";
+import {
+  Routes,
+  APIGuildChannel,
+  ChannelType,
+  APIGuild,
+} from "discord-api-types/v10";
 
 const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID;
 if (!DISCORD_GUILD_ID) {
   throw new Error("Missing DISCORD_GUILD_ID");
 }
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
-
-async function connectClient() {
-  if (!client.isReady()) {
-    await client.login(process.env.DISCORD_BOT_TOKEN);
-
-    if (!client.isReady()) {
-      await new Promise((resolve) => client.once("ready", resolve));
-    }
-  }
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+if (!DISCORD_BOT_TOKEN) {
+  throw new Error("Missing DISCORD_BOT_TOKEN");
 }
 
-export async function getChannels() {
-  await connectClient();
+const rest = new REST({ version: "10", authPrefix: "Bot" }).setToken(
+  DISCORD_BOT_TOKEN
+);
 
-  const channelsBad = client.channels.cache.values();
+export async function getChannels() {
+  if (!DISCORD_GUILD_ID) {
+    throw new Error("Missing DISCORD_GUILD_ID");
+  }
+
+  const channelsRaw = (await rest.get(
+    Routes.guildChannels(DISCORD_GUILD_ID)
+  )) as APIGuildChannel<ChannelType>[];
 
   const channels: { name: string; size: number }[] = [];
-  for (const channel of channelsBad) {
-    const c = (await channel.fetch()) as GuildChannel;
-
-    if (c.parentId && !c.isThread()) {
-      channels.push({ name: c.name, size: c.members.size });
+  for (const channel of channelsRaw) {
+    if (
+      channel.parent_id &&
+      (channel.type === ChannelType.GuildText ||
+        channel.type === ChannelType.GuildAnnouncement ||
+        channel.type === ChannelType.GuildForum)
+    ) {
+      channels.push({ name: channel.name, size: 0 });
     }
   }
 
@@ -45,12 +53,7 @@ export async function getMemberCount() {
     throw new Error("Missing DISCORD_GUILD_ID");
   }
 
-  await connectClient();
+  const guild = (await rest.get(Routes.guild(DISCORD_GUILD_ID))) as APIGuild;
 
-  const guild = client.guilds.cache.get(DISCORD_GUILD_ID);
-  if (!guild) {
-    throw new Error("Guild not found");
-  }
-
-  return guild.memberCount;
+  return guild.approximate_member_count ?? 0;
 }
